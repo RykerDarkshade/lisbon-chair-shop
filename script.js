3cd1a737127142882ccee89148eab3c32ab90975
Deuel Ramos <deuelramos@gmail.com>
2024-02-12 11:00:57 -0400

    V 2.5
    - Added Carrousel
    - Added HMTL Id's for JS

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 "use strict";
 
+// Carousel
+const btnRight = document.querySelector(".btn--right");
+const btnLeft = document.querySelector(".btn--left");
+const unit1 = document.getElementById("unit-1");
+const unit2 = document.getElementById("unit-2");
+
+btnRight.addEventListener("click", function () {
+  unit1.classList.toggle("open");
+  unit2.classList.toggle("open");
+});
+
+btnLeft.addEventListener("click", function () {
+  unit1.classList.toggle("open");
+  unit2.classList.toggle("open");
+});
+
 // Accordion
-const item1 = document.querySelector(".item--1");
-const item2 = document.querySelector(".item--2");
-const item3 = document.querySelector(".item--3");
-const btn1 = document.querySelector(".acc-1");
-const btn2 = document.querySelector(".acc-2");
-const btn3 = document.querySelector(".acc-3");
+const btn1 = document.getElementById("acc-1");
+const btn2 = document.getElementById("acc-2");
+const btn3 = document.getElementById("acc-3");
 
 btn1.addEventListener("click", function () {
-  item1.classList.toggle("open");
+  this.parentElement.classList.toggle("open");
 });
 
 btn2.addEventListener("click", function () {
-  item2.classList.toggle("open");
+  this.parentElement.classList.toggle("open");
 });
 
 btn3.addEventListener("click", function () {
-  item3.classList.toggle("open");
+  this.parentElement.classList.toggle("open");
 });
